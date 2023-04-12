043ebddcde834e045bf7b07f6d9bc4214de94efe
jay002200@gmail.com <Yujie>
2023-04-12 17:45:12 +0800

    new day new egg

--- egg.py
+++ egg.py
@@ -2,7 +2,7 @@ import cv2
 from PIL import ImageFont, ImageDraw, Image
 import numpy as np
 import pygsheets
-
+import datetime
 
 
 def make_egg_pic(text1,text2,text3):
@@ -12,24 +12,24 @@ def make_egg_pic(text1,text2,text3):
     allegg += text2
     time = "更新時間:\n"
     time += text3
-    img = cv2.imread("blackboard.png")
+    img = cv2.imread("/home/-")
     imgPil = Image.fromarray(img)
-    fontPath = "./font.ttf"
+    fontPath = "/home/-"
     font = ImageFont.truetype(fontPath,110)
     draw = ImageDraw.Draw(imgPil)
-    draw.text((93,37),text,font=font,fill=(0,0,255))
-    draw.text((93,152),allegg,font=font,fill=(0,0,255))
-    font = ImageFont.truetype(fontPath,60)
-    draw.text((93,288),time,font=font,fill=(0,0,255))
+    draw.text((93,37),text,font=font,fill=(255,0,255))
+    draw.text((93,152),allegg,font=font,fill=(255,0,255))
+    font = ImageFont.truetype(fontPath,55)
+    draw.text((93,288),time,font=font,fill=(255,0,255))
     img = np.array(imgPil)
-    path = "egg.jpg"
+    path = "/home/test/egg.jpg"
     cv2.imwrite(path,img)
     
 def googlesheet():
-    auth_file = ".json"
+    auth_file = "/home/-"
     gc = pygsheets.authorize(service_file = auth_file)   
     # setting sheet
-    sheet_url = "https://docs.google.com/spreadsheets/d/-" 
+    sheet_url = "https://docs.google.com/-" 
     sheet = gc.open_by_url(sheet_url)
     sheet_test01 = sheet.worksheet_by_title("工作表1")
     O2 = sheet_test01.cell('O2')  #今日產蛋
@@ -39,6 +39,12 @@ def googlesheet():
     data.append(O2.value)
     data.append(P2.value)
     data.append(Q2.value)
+    
+    #全新的一天 初始化產蛋
+    now = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=8)))
+    hours = now.strftime('%H')
+    if hours == "00":
+    	sheet_test01.update_value("O2","0")
     return data
 
 data = googlesheet()
